b6a754fdcc8077cf6ccb56766335b81eaf79d87c
Kiante Fernandez <kiante27@yahoo.com>
2022-07-10 10:37:34 +0200

    set up the env for seesion 1

---
+++ day_1/cmmc2022_python_example.py
@@ -0,0 +1,20 @@
+# Python Script Example: 2022 Computational and Mathematical Modeling of Cognition
+#
+# Record of Revisions
+#
+# Date            Programmer              Descriptions of Change
+# ====         ================           ======================
+# 09-July-22        Michael Nunez               Original code
+
+import os
+import numpy as np
+import pandas as pd
+
+# Q1.0
+# This is the answer the the example problem Q1.0 in a comment
+this_variable = 1
+print(this_variable)
+
+# Out[3]: 1
+
+# Q1.1
---
+++ day_1/cmmc2022_session1_lecture.py
@@ -0,0 +1,74 @@
+# Session 1: Introduction to Python Lecture - 2022 Computational and Mathematical Modeling of Cognition
+#
+# Record of Revisions
+#
+# Date            Programmer              Descriptions of Change
+# ====         ================           ======================
+# 08-July-22     Michael D. Nunez              Original code
+
+import os
+import numpy as np
+import pandas as pd
+
+# Open IPython
+# pwd in IPython
+# ?os in IPython
+# os.chdir('..')
+# os.chdir('C:/Users') # For Windows, other methods are poor
+# os.chdir('/home/') # For Mac ?
+
+# Make an array
+my_array = np.array([2, 3, 4])
+
+# %paste in Python
+# Talk about %paste versus CTRL+C
+
+# What is the size of my array?
+# Sometimes there are multiple ways of doing things (see Python Class discussion later today)
+my_array.shape
+np.shape(my_array)
+
+# Reading errors!
+# MY_array # Ask why?
+
+# Python indexing
+# Welcome to 0 indexing!
+# What does each one do?
+my_array[0]
+my_array[1]
+my_array[-1]
+my_array[0:1]
+my_array[0:2]
+my_array[-1:]
+
+# Printing multiple lines
+print(my_array[0])
+print(my_array[1])
+
+# Approach to errors, this just happened to me
+copied_array = my_array
+copied_array[0] = np.nan
+
+# Talk about Python types
+print(my_array.dtype)
+my_array = my_array.astype('float64')
+my_array[0] = np.nan
+
+# Python fun with new copies
+copied_array = my_array
+copied_array[1] = np.nan
+print(copied_array)
+print(my_array)
+
+# A possibly better way
+truly_copied = np.copy(my_array)
+truly_copied[2] = np.nan
+print(truly_copied)
+print(my_array)
+
+# Pandas data frames
+all_data = {"my_array": my_array, "copied_array": copied_array,
+            "truly_copied": truly_copied}
+my_data = pd.DataFrame(all_data)
+print(my_data)
+
